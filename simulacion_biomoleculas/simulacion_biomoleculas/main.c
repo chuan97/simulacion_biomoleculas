f4f13cbe86931d2783b644fdb63ee7410315ad7f
Fernando Loren <fernando-loren@hotmail.com>
2018-10-02 23:41:30 +0200

    Primera prueba

--- simulacion_biomoleculas/simulacion_biomoleculas/main.c
+++ simulacion_biomoleculas/simulacion_biomoleculas/main.c
@@ -2,7 +2,7 @@
 //  main.c
 //  simulacion_biomoleculas
 //
-//  Created by Juan Román Roche on 02/10/2018.
+//  Created by Juan Román Roche & Fernando Lorén Mastral on 02/10/2018.
 //  Copyright © 2018 Grupo_5. All rights reserved.
 //
 
