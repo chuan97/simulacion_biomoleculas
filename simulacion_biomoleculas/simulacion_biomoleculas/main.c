92585eb54089a60e985e5e704176106f0b659d3c
Fernando Loren <fernando-loren@hotmail.com>
2018-10-08 17:19:01 +0200

    hi

--- simulacion_biomoleculas/simulacion_biomoleculas/main.c
+++ simulacion_biomoleculas/simulacion_biomoleculas/main.c
@@ -15,23 +15,23 @@
 
 //he definido unas constantes pero tengo infinitas dudas con esto
 #define h 0.1
-#define k 1
-#define m 1
-#define T 1
-#define nu 10
-#define kb 1
-#define c0 ( 2 * nu * kb * T )
-#define x0 100
-#define v0 0
+#define k 1.0
+#define m 1.0
+#define T 1.0
+#define nu 0.01
+#define kb 1.0
+#define c0 ( 2.0 * nu * kb * T )
+#define x0 100.0
+#define v0 0.0
 #define n_steps 100000
 
 //Parámetros del Runge Kutta
 #define A1 0.5
 #define A2 0.5
-#define beta 1
-#define lambda0 1
-#define lambda1 1 // también se puede usar lambda1=0 y lambda2=1
-#define lambda2 0
+#define beta 1.0
+#define lambda0 1.0
+#define lambda1 1.0 // también se puede usar lambda1=0 y lambda2=1
+#define lambda2 0.0
 
 // Algoritmo a usar
 //#define Euler_RK
@@ -47,16 +47,16 @@ double rdm(void){
 
 //algoritmo de box-muller, si vemos que no usamos los dos numeros podemos quitar uno y así optimizamos y lo sacamos con un return
 void gauss(double* g1, double* g2){
-    double root = sqrtf(-1 * 2 * logf(rdm()));
-    double arg = 2 * M_PI * rdm();
+    double root = sqrt(-1.0 * 2.0 * log(rdm()));
+    double arg = 2.0 * M_PI * rdm();
 
-    *g1 = -1 * root * cosf(arg);
-    *g2 = -1 * root * sinf(arg);
+    *g1 = -1.0 * root * cos(arg);
+    *g2 = -1.0 * root * sin(arg);
 }
 
 //funcion fuerza, está aparte para poder irla cambiando
 double force(double x, double t){
-    return -1 * k * x;
+    return -1.0 * k * x;
 }
 
 //integración por euler maruyama
@@ -65,7 +65,7 @@ double Euler_maru(double t_prev, double x_prev){
 
     gauss(&g1, &g2);
 
-    return x_prev + h * force(x_prev, t_prev) + sqrtf(c0 * h) * g1;
+    return x_prev + h * force(x_prev, t_prev) + sqrt(c0 * h) * g1;
 }
 
 //Integración por Runge-Kutta(2o orden)
@@ -74,23 +74,24 @@ double Runge_kutta2(double t_prev, double x_prev){
 
     gauss(&Z1, &Z2);
 
-    g1=force(x_prev + sqrtf(c0 * h) * lambda1 * Z1, t_prev);
-    g2=force(x_prev + beta * h * g1 + sqrtf(c0 * h) * lambda2 * Z1, t_prev);
+    g1=force(x_prev + sqrt(c0 * h) * lambda1 * Z1, t_prev);
+    g2=force(x_prev + beta * h * g1 + sqrt(c0 * h) * lambda2 * Z1, t_prev);
 
-    return x_prev + h * ( A1 * g1 + A2 * g2 ) + sqrtf(c0 * h) * lambda0 * Z1;
+    return x_prev + h * ( A1 * g1 + A2 * g2 ) + sqrt(c0 * h) * lambda0 * Z1;
 }
 
 //Integración por Verlet Explicito
 void Verlet_exp(double t_prev, double x_prev, double v_prev, double* t_next, double* x_next, double* v_next){
     double g1, g2, a, b;
     *t_next = t_prev + h;
+
     gauss(&g1, &g2);
 
-    a = (1 - 0.5 * nu * h / m ) / (1 + 0.5 * nu * h / m) ;
-    b = 1.0 / (1 + 0.5 * nu * h / m) ; // no estoy seguro si el .0 hace falta
+    a = (1.0 - 0.5 * nu * h / m ) / (1.0 + 0.5 * nu * h / m) ;
+    b = 1.0 / (1.0 + 0.5 * nu * h / m) ; // no estoy seguro si el .0 hace falta
 
-    *x_next = x_prev + b * h * v_prev + 0.5 * b * h * h * force(x_prev, t_prev) / m + 0.5 * b * h * g1 / m ;
-    *v_next = a * v_prev + 0.5 * h * ( a * force(x_prev, t_prev) + force(*x_next, *t_next)) / m +  b * g1 / m ; // no tengo claro si el número aleatorio tiene que ser el mismo o no
+    *x_next = x_prev + b * h * v_prev + 0.5 * b * h * h * force(x_prev, t_prev) / m + 0.5 * b * h * c0 * g1 / m ; // se multiplican por c0 los numeros aleatorios para que se cumplan las condiciones estadisticas del metodo
+    *v_next = a * v_prev + 0.5 * h * ( a * force(x_prev, t_prev) + force(*x_next, *t_next)) / m +  b * c0 * g1 / m ; // no tengo claro si el número aleatorio tiene que ser el mismo o no
 
 }
 
@@ -104,13 +105,13 @@ void save_trajectory(double* t, double* x, double* v){
     /// Guardar trayectoria para Euler-Maruyama y RK2
     #ifdef Euler_RK
     for (i = 0; i < n_steps; i++){
-        fprintf(f, "%f %f\n", t[i], x[i]);
+        fprintf(f, "%lf %lf\n", t[i], x[i]);
     }
     #endif
     /// Guardar trayectoria (con velocidades) para Verlet explicito
     #ifdef Verlet
     for (i = 0; i < n_steps; i++){
-        fprintf(f, "%f %f %f\n", t[i], x[i], v[i]);
+        fprintf(f, "%lf %lf %lf\n", t[i], x[i], v[i]);
     }
     #endif
 
@@ -125,14 +126,14 @@ int main(int argc, const char* argv[]) {
     double t[n_steps];
     double v[n_steps];
 
-    t[0] = 0;
+    t[0] = 0.0;
     x[0] = x0;
     v[0] = v0;
 
     /// Calcula la trayectoria de Euler-Maruyama o RK2
     #ifdef Euler_RK
     for (i = 1; i < n_steps; i++){
-        x[i] = Runge_kutta2(t[i - 1], x[i - 1]);
+        x[i] = Euler_maru(t[i - 1], x[i - 1]);
         t[i] = t[i - 1] + h;
     }
     #endif
